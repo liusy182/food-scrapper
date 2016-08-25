2f409c9f874b6375d80f714877a98861230b099e
liusy182 <liusy182@gmail.com>
2016-08-25 21:14:46 +0800

    data reading

--- openrice.js
+++ openrice.js
@@ -16,20 +16,61 @@ function getNextPageLinks() {
   });
 }
 
-
 function openrice(casper) {
-  let pageLinks = [];
-  casper.start('http://sg.openrice.com/en/singapore/restaurants')
+  casper.start('http://sg.openrice.com/en/singapore/restaurants?page=15')
     .then(scrapNextPage);
 
   function scrapNextPage() {
     let links = this.evaluate(getChildPageLinks);
-    //pageLinks = pageLinks.concat(links);
 
-    links.map(function(l) {
-      fs.write('urls.txt', l, 'a');
-      fs.write('urls.txt', '\n', 'a');
-    }.bind(this));    
+    casper.start().each(links, function(self, link) {
+      self.thenOpen(link, function() {
+        let info = [];
+
+        let name = this.fetchText('.restaurant-title-lang1')
+          .replace(/\n/g, ' ').replace(/,/g, ' ').replace(/ +/g, ' ').trim();
+        info.push(name);
+
+        let address = this.fetchText('.address-info-section .text')
+          .replace(/\n/g, ' ').replace(/,/g, ' ').replace(/ +/g, ' ').trim();
+        info.push(address);
+
+        let phone = this.fetchText('.sr2-overview-container.phone-section a')
+          .replace(/\n/g, ' ').replace(/,/g, ' ').replace(/ +/g, ' ').trim();
+        info.push(phone);
+
+        let style = this.fetchText('.sr2-overview-container .text.comma-tags')
+          .replace(/\n/g, ' ').replace(/,/g, ' ').replace(/ +/g, ' ').trim();
+        info.push('style');
+
+        let budget = this.fetchText('.sr2-overview-container .text[itemprop=priceRange]')
+          .replace(/\n/g, ' ').replace(/,/g, ' ').replace(/ +/g, ' ').trim();
+        info.push(budget);
+
+        let openingHour = this.fetchText({
+          type: 'xpath',
+          path: '//*[@class="or-sprite condition_time_40x40"]/../..'
+        }).replace(/\n/g, ' ').replace(/,/g, ' ').replace(/ +/g, ' ').trim();
+        info.push(openingHour);
+
+        let website = this.fetchText('.restaurant-url .text')
+          .replace(/\n/g, ' ').replace(/,/g, ' ').replace(/ +/g, ' ').trim();
+        info.push(website);
+
+        this.echo('=================================');
+        this.echo('name: ' + name);
+        this.echo('address: ' + address);
+        this.echo('phone: ' + phone);
+        this.echo('style: ' + style);
+        this.echo('budget: ' + budget);
+        this.echo('openingHour: ' + openingHour);
+        this.echo('website: ' + website);
+        this.echo('=================================');
+
+        fs.write('result.txt', info.join(','), 'a');
+        fs.write('urls.txt', '\n', 'a');
+      });
+    });
 
     let nextPageLink = this.evaluate(getNextPageLinks);
     if(nextPageLink.length > 0) {
@@ -38,11 +79,6 @@ function openrice(casper) {
       casper.then(scrapNextPage)
     }
   }
-
-  // casper.then(function() {
-  //   // aggregate results for the 'phantomjs' search
-  //   this.echo('outcome: ' + pageLinks.length + '\n' + pageLinks.join('\n'));
-  // });
 }
 
 
